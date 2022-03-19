39f0bd2ba2e8c4158c7a67725de2a1e5ada6267f
Swarsame <user@Users-MacBook-Air.local>
2022-03-18 22:32:50 -0600

    created a loop

---
+++ W9C/app.js
@@ -0,0 +1,10 @@
+var tweets = ["Hey guys" , "I hope 2022 is nothing like 2021 " , "Lebron James is only 20 points away from being the No.2 All-time leading scorer in NBA history" , "Nicki minaj dropped a new song" , "I need about 200billion dollars " , "Going on a date kinda nervous lol" , "Get lost in what you love " , "Strong people don/'t put others down , they lift them up " , "Just do it " , "Hard work pays off" 
+];
+console.log(tweets);
+
+// creating a loop
+ 
+for (var counter = 0; counter < tweets.length; counter= counter +1) {
+    console.log(tweets[counter]);
+    
+}
