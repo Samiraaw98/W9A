37435502235959e4280a9d51de20e3a4a7a1755d
Swarsame <user@Users-MacBook-Air.local>
2022-03-18 22:08:28 -0600

    W9B

---
+++ W9B/app.js
@@ -0,0 +1,49 @@
+console.log("Hello");
+
+
+var is_subscribed = true;
+var userAge = [ "21" , "20" , "15" ,"16" ,"14"];
+console.log(userAge);
+var subscribed = [true , false];
+console.log(subscribed);
+
+
+var userAge =21;
+console.log(userAge);
+if (userAge > 18) {
+console.log("User is old enough ");
+} else  (is_subscribed=true) 
+    console.log("User is NOT subscribed");
+
+
+    var userAge =20;
+    console.log(userAge);
+    var is_subscribed = true;
+    if (userAge > 18) {
+  console.log("User is  old enough ");
+  } else  (is_subscribed=false) 
+ console.log("User is NOT subscribed");
+
+  var userAge =15;
+  console.log(userAge);
+ var is_subscribed = true;
+ if (userAge < 18) {
+    console.log("User is  NOT old enough ");
+    } else  (is_subscribed=true) 
+        console.log("User is  subscribed");
+        
+
+ var userAge =22;
+ console.log(userAge);
+  var is_subscribed = true;
+
+  
+ if (userAge > 18) {
+    console.log("User is   old enough ");
+    } else  (is_subscribed=true) 
+        console.log("User is  subscribed");
+
+for (var counter = 0; counter < userAge.length; counter+1) {
+    console.log(userAge[counter]);
+    
+}
