a79d14c346c7de4bc812691dec2a5668476bdebf
Swarsame <user@Users-MacBook-Air.local>
2022-03-25 23:56:08 -0600

    W10B

---
+++ W10B/app.js
@@ -0,0 +1,7 @@
+// creating a cookie 
+
+function foodSelection(){
+    var option  = document.getElementById("food1").value;
+    Cookies.set("choose" , "tacos");
+
+}
---
+++ W10B/selection.js
@@ -0,0 +1,12 @@
+// reading the cookie
+
+let option = Cookies.get("choose");
+document.getElementById("food1");
+  console.log("tacos");
+
+
+
+
+
+
+
